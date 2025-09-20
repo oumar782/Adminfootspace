09500ccc929ed75a553daab4b28b666e9e7cada3
Oumar Diane <oumardiane399@gmail.com>
2025-09-20 22:34:25 +0200

    jngjhn

--- src/Page/calendriers.jsx
+++ src/Page/calendriers.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Calendrier.css';
+import '../cal/Calendrier.css';
 
 // Composant Toast pour les retours utilisateur
 const CalLuxeToast = ({ message, type, onClose }) => {
