8cf8d864c610327df72c406ff33a8e95dced5d7d
Oumar Diane <oumardiane399@gmail.com>
2025-09-25 17:52:26 +0200

    djng

--- src/layout/Mainlayout.jsx
+++ src/layout/Mainlayout.jsx
@@ -2,7 +2,8 @@ import { ReactNode, useState } from "react";
 
 import Sidebar from "../composant/Sidebar";
 import Navbar from "../composant/Navbar";
-import "../layout/Mainlayout.css";
+import "./Mainlayout.css";
+
 
 const MainLayout = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
